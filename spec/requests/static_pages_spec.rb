b52c8b61d4baac68b2497d26d35c14e534d41423
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-06-24 16:56:28 +0900

    First staticpages, first test

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "Static pages" do
+  describe "Home page" do
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('comet')
+    end
+  end
+end
