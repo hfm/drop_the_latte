97ad3be2d06729abeb88398f6f0ca289fb0a646b
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-06-24 15:43:26 +0900

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails', '~> 4.0.0.rc2'
+gem 'mysql2'
+
+group :development do
+  gem 'sqlite3', '1.3.7'
+end
+
+gem 'sass-rails', '~> 4.0.0.rc2'
+gem 'uglifier', '~> 2.1.1'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  gem 'sdoc', '0.3.20', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DropTheLatte::Application.config.secret_key_base = '67f1997dd8afcba8109ec58d42b281dcb61e92056287a69959c17e50fbc998651c7184f5956cd1d8021ffa908c243545cd56cabfd2d5f94178894b878ae49b63'
