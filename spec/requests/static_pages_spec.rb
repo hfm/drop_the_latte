a0588546160b0fe8da627035b28ec4b31822d78f
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-06-24 17:18:27 +0900

    Add a test for Page title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  let(:base_title) { "comet" }
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('comet')
+    end
+
+    it "should have the title 'comet'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 end
