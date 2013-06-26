df253438a2841d53e06d658b998a109b15c0ab5e
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-06-26 18:16:52 +0900

    Mockup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130626021316) do
+ActiveRecord::Schema.define(version: 20130626075221) do
 
   create_table "comments", force: true do |t|
     t.string   "content"
@@ -29,8 +29,10 @@ ActiveRecord::Schema.define(version: 20130626021316) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "other_id"
   end
 
+  add_index "photos", ["other_id"], name: "index_photos_on_other_id"
   add_index "photos", ["user_id", "created_at"], name: "index_photos_on_user_id_and_created_at"
 
   create_table "users", force: true do |t|
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -100,6 +100,18 @@ describe "Authentication" do
           specify { expect(response).to redirect_to(signin_path) }
         end
       end
+
+      describe "in the Comments controller" do
+        describe "submitting to the create action" do
+          before { post comments_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete photo_path(FactoryGirl.create(:comment, user_id:user.id)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
     end
 
     describe "as non-admin user" do
