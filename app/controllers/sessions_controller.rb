5cd911c4c25b46eb8aa7e72a5b216d5160eda9e9
Takahiro OKUMURA <hfm.garden@gmail.com>
2013-06-27 15:38:32 +0900

    Change form_tag to form_for

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email].downcase)
-    if user && user.authenticate(params[:password])
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
       sign_in user
       redirect_back_or user
     else
-      flash.now[:error] = 'Invalid email/password combination'
+      flash.now[:error] = 'メールまたはパスワードが違います'
       render 'new'
     end
   end
